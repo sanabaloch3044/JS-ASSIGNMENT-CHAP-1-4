5c2a8f5ee24893a8e27d75950bb73f57318b471c
sana <sanawbalochh304@gmail.com>
2024-07-02 14:31:01 -0700

    changes

---
+++ index.js
@@ -0,0 +1,100 @@
+//-----------------------CHAPTER 1
+
+alert("Hey World !");
+alert("Welcome to my page !");
+alert("Let me introduce myself.");
+alert("I am Sana.");
+alert(" I am a student.");
+alert("I am learning web development.");
+alert("I choose this field,");
+alert("Because i'm crazy about this field.");
+alert("I am doing hard work on this.");
+alert("INSHALLAH I will acheive my all dreams one day !");
+console.log(alert);
+
+
+
+//-----------------------CHAPTER 2
+
+
+var userName;
+var myName = "Sana Baloch"
+console.log(myName)
+
+var message = "Hello World !"
+alert(message)
+
+
+var studentName = "Sahil"
+var studentAge = "13 years old"
+var studentAcheviement = "Certified Graphic Designer"
+alert(studentName);
+alert(studentAge);
+alert(studentAcheviement)
+
+var food = "PIZZA\nPIZZ\nPIZ\nPI\nP" 
+alert(food);
+
+var email = "sanawbalochh304@gmail.com"
+alert("My email address is " + email);
+
+var book = "A Smarter way to learn JavaScript."
+alert("I am trying to learn from the book " + book );
+
+document.write("Yah ! I can write HTML content through javaScript" + "<br>");
+
+var symbolss = "“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”"
+alert(symbolss);
+
+
+//-----------------------CHAPTER 3
+
+
+ var age = 23
+alert("I am " + age + " years old !");
+
+
+var vistor;
+vistor = 20 
+
+alert("You have visited this site " + vistor + " times.");
+
+
+var birthYear = 1999
+document.write("My birth Year is " + birthYear + "<br>")
+document.write("Data type of my declared variable is number " + "<br>");
+
+var vistorName = "Shaheena"
+var productTitle = "Long Shirt"
+var quantity = 3
+document.write(vistorName + " ordered " + quantity +" " + productTitle + " on khadi's clothing store");
+
+
+
+//------------------------------CHAPTER 4
+
+
+var student, teacher, classroom;
+
+// 5 LEGAL VARIABLE
+
+/*firstName //camelCase
+_age // underscore
+$amount // dollar sign
+user123 // number with any name
+AI // Uppercase
+
+// 5 ILLEGAL VARIABLE
+
+3rdName   // start with number
+my-home  // contain hyphen
+let/ var / const // reserved keywords of js
+var#amount // contain an invalid character 
+class // reversed keyword in js*/
+
+
+document.write('<h1>' + "Rules for naming JS variables" + '</h1>' + '<br>'+ '<br>');
+document.write('variable names can only contain , number, $ , and _ . For example $my_1stVariable ' + "<br>" );
+document.write("Variables must begin with a letter, $ or _. For example $name, _name or name" + "<br>");
+document.write("Variable names are case sensitive" + "<br>");
+document.write("Variable names should not be JS keywords");
